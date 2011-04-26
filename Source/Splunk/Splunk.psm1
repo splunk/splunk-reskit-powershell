237973f9839cabf3b9becd89a80f4a40a15ec85b
Brandon Shell <tshell@gmail.com>
2011-04-26 17:11:26 -0400

    Added Set-SplunkdLogging

--- Source/Splunk/Splunk.psm1
+++ Source/Splunk/Splunk.psm1
@@ -334,7 +334,7 @@ function Invoke-SplunkAPIRequest
     Write-Verbose " [Invoke-SplunkAPIRequest] ::  - Protocol     = $Protocol"
     Write-Verbose " [Invoke-SplunkAPIRequest] ::  - Timeout      = $Timeout"
     
-    $FullURL = "{0}://{1}:{2}/{3}" -f $Protocol,$ComputerName,$Port,($Endpoint -replace '^/(.*)','$1').ToLower()
+    $FullURL = "{0}://{1}:{2}/{3}" -f $Protocol,$ComputerName,$Port,($Endpoint -replace '^/(.*)','$1')
     Write-Verbose " [Invoke-SplunkAPIRequest] ::  - FullURL      = $FullURL"
 	
 	$InvokeHTTPParams = @{
@@ -2115,6 +2115,119 @@ function Get-SplunkdLogging
 } # Get-SplunkdLogging
 
 #endregion Get-SplunkdLogging
+
+#region Set-SplunkdLogging
+
+function Set-SplunkdLogging # Need to note Change does not persist service restart
+{
+	[Cmdletbinding(SupportsShouldProcess=$true,DefaultParameterSetName="byFilter")]
+    Param(
+    
+		[Parameter(ValueFromPipeline=$true,Position=0,ParameterSetName="byLogger")]
+		[Object]$Logger,
+		
+        [Parameter(Position=0,ParameterSetName="byFilter")]
+        [STRING]$Filter = '.*',
+	
+		[Parameter(Position=0,ParameterSetName="byName")]
+		[STRING]$Name,
+        
+        [Parameter()]        
+        [ValidateSet("WARN" , "DEBUG" , "INFO" , "CRIT" , "ERROR" , "FATAL")]
+		[STRING]$NewLevel,
+	
+        [Parameter(ValueFromPipelineByPropertyName=$true,ValueFromPipeline=$true)]
+        [String]$ComputerName = $SplunkDefaultObject.ComputerName,
+        
+        [Parameter()]
+        [int]$Port            = $SplunkDefaultObject.Port,
+        
+        [Parameter()]
+		[ValidateSet("http", "https")]
+        [STRING]$Protocol     = $SplunkDefaultObject.Protocol,
+        
+        [Parameter()]
+        [int]$Timeout         = $SplunkDefaultObject.Timeout,
+
+        [Parameter()]
+        [System.Management.Automation.PSCredential]$Credential = $SplunkDefaultObject.Credential
+        
+    )
+	
+	Begin
+	{
+		Write-Verbose " [Set-SplunkdLogging] :: Starting..."
+        $ParamSetName = $pscmdlet.ParameterSetName
+        
+        switch ($ParamSetName)
+        {
+            "byFilter"  { $LoggerObjects = Get-SplunkdLogging -Filter $Filter	} 
+            "byName"    { $LoggerObjects = Get-SplunkdLogging -Name $Name 		}
+        }
+        
+	}
+	Process
+	{
+		Write-Verbose " [Set-SplunkdLogging] :: Parameters"
+        Write-Verbose " [Set-SplunkdLogging] ::  - ParameterSet = $ParamSetName"
+		Write-Verbose " [Set-SplunkdLogging] ::  - ComputerName = $ComputerName"
+		Write-Verbose " [Set-SplunkdLogging] ::  - Port         = $Port"
+		Write-Verbose " [Set-SplunkdLogging] ::  - Protocol     = $Protocol"
+		Write-Verbose " [Set-SplunkdLogging] ::  - Timeout      = $Timeout"
+		Write-Verbose " [Set-SplunkdLogging] ::  - Credential   = $Credential"
+        Write-Verbose " [Set-SplunkdLogging] ::  - LevelFilter  = $LevelFilter"
+        Write-Verbose " [Set-SplunkdLogging] ::  - WhereFilter  = $WhereFilter"
+
+		if($Logger -and $Logger.PSTypeNames -contains "Splunk.SDK.Splunkd.Logger")
+		{
+			$LoggerObjects = $Logger
+		}
+		
+		foreach($LoggerObject in $LoggerObjects)
+		{
+
+			Write-Verbose " [Set-SplunkdLogging] :: Setting up Invoke-APIRequest parameters"
+			$InvokeAPIParams = @{
+				ComputerName = $ComputerName
+				Port         = $Port
+				Protocol     = $Protocol
+				Timeout      = $Timeout
+				Credential   = $Credential
+				Endpoint     = $LoggerObject.ServiceURL
+				Verbose      = $VerbosePreference -eq "Continue"
+			}
+			$Arguments = @{"level"=$NewLevel}
+			
+            Write-Verbose " [Set-SplunkdLogging] :: Using endpoint $($LoggerObject.ServiceURL)"
+			Write-Verbose " [Set-SplunkdLogging] :: Calling Invoke-SplunkAPIRequest @InvokeAPIParams"
+			try
+			{
+				if($Force -or $PSCmdlet.ShouldProcess($ComputerName,"Setting Splunkd Logging [$($LoggerObject.Name)] to [$NewLevel]"))
+				{
+					[XML]$Results = Invoke-SplunkAPIRequest @InvokeAPIParams -Arguments $Arguments -RequestType POST
+					if($Results -and ($Results -is [System.Xml.XmlDocument]))
+					{
+						Get-SplunkdLogging -Name $LoggerObject.Name
+					}
+					else
+					{
+						Write-Verbose " [Set-SplunkdLogging] :: No Response from REST API. Check for Errors from Invoke-SplunkAPIRequest"
+					}
+				}
+			}
+			catch
+			{
+				Write-Verbose " [Set-SplunkdLogging] :: Invoke-SplunkAPIRequest threw an exception: $_"
+			}
+		}
+	}
+	End
+	{
+		Write-Verbose " [Set-SplunkdLogging] :: =========    End   ========="
+	}
+} # Set-SplunkdLogging
+
+#endregion Set-SplunkdLogging
 
 #endregion SplunkD
 
