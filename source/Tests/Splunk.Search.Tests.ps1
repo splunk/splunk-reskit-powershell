dcbe24c60178947e683da703f06f286b125b4c59
unknown <beefarino@.(none)>
2011-06-16 17:05:47 -0400

    added search test file

---
+++ source/Tests/Splunk.Search.Tests.ps1
@@ -0,0 +1,17 @@
+﻿param( $fixture )
+
+Describe "search-splunk" {
+	
+	It "returns results on a valid search using default connection parameters" {
+		$results = search-splunk -search "s"
+		
+		return [bool]$results;
+	}	
+
+	It "does not raise exception when empty set is returned" {
+		$e = @();
+		search-splunk -search "thisshouldreturnanemptyset" -erroraction 'silentlycontinue' -errorvariable e
+		$e | Write-Host;
+		return -not $e;
+	}	
+}
