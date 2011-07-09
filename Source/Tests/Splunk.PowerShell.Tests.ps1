c90a3d587b4628c5a338358855790e887134d1c2
beefarino <jimchristopher@gmail.com>
2011-07-08 21:51:16 -0400

    fixed missing help topics and parameter entries; added unit tests to find missing parameter entries

--- Source/Tests/Splunk.PowerShell.Tests.ps1
+++ Source/Tests/Splunk.PowerShell.Tests.ps1
@@ -2,8 +2,9 @@
 
 Get-Command -Module splunk | foreach {
 	$script:this = $_;
-	Describe "Splunk Module" {		
-		It "$($script:this.Name) has custom help" {
+	$script:commandName = $_.Name;
+	Describe $script:commandName {		
+		It "has custom help" {
 			$script:this | Write-Debug;
 			$local:help = ( $script:this | Get-Help -full ) | Out-String;
 			
@@ -12,6 +13,33 @@ Get-Command -Module splunk | foreach {
 				$local:help -match 'SYNTAX' -and
 				$local:help -match 'DESCRIPTION' -and
 				$local:help -match 'EXAMPLE';
+		}
+			
+		$commonParameterNames = @"
+			Verbose
+			Debug
+			ErrorAction
+			WarningAction
+			ErrorVariable
+			WarningVariable
+			OutVariable
+			OutBuffer
+			UseTransaction
+"@ -split '\s+'; 
+
+		$script:this | select -exp parameters | select -expand keys | where {
+			$commonParameterNames -notcontains $_
+		} | foreach {	
+			It "has help for parameter $_" {
+				$paramNames = $script:this | `
+					Get-Help -full | `
+					select -exp parameters | `
+					select -exp parameter | `
+					select -exp name;
+					
+				$paramNames -contains $_;
+				
+			}
 		}
 
 	}
